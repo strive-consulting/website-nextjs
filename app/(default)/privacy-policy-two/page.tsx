c9b1810455a89ab7a73d7d842a3dcbe452ba5827
Raj Karwal <karwalr@hotmail.com>
2023-06-12 21:30:26 +0100

    Type fix

--- app/(default)/privacy-policy-two/page.tsx
+++ app/(default)/privacy-policy-two/page.tsx
@@ -1,5 +1,5 @@
 
-const getPosts = async (): Promise<Post[]> => {
+const getPosts = async (): Promise<any[]> => {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await data.json();
 
