ae8fb703c6a4f2996c0e5b3196a75e389cb00f28
strivenabil <nabil@strive.ae>
2024-10-22 19:30:36 +0400

    SEO Titles updated

--- app/(default)/tools/business-name-checker/page.tsx
+++ app/(default)/tools/business-name-checker/page.tsx
@@ -5,8 +5,8 @@ import CompanyNameChecker from '@/components/calculators/company-name-checker'
 
 export const metadata: Metadata = {
   robots: { index: true, follow: true },
-  title: 'Dubai Business Name Checker',
-  description: 'Dubai Business Name Checker by Strive. Use our tool to check if your new business name is available in the UAE',
+  title: 'Business Name Checker for Setting up a Company in Dubai',
+  description: 'Use Strive’s business name checker to see if your company name is available in Dubai. Start your business setup with the right name in the UAE.',
   alternates: {
     canonical: Constants.SiteDomain + '/tools/business-name-checker',
   },
@@ -41,7 +41,7 @@ export default async function Home() {
           <div className='pt-32 pb-12 md:pt-40 md:pb-20'>
             {/* Page header */}
             <div className='max-w-3xl mx-auto text-center mb-5'>
-              <h1 className='h1 mb-4'>UAE Business Name Checker</h1>
+              <h1 className='h1 mb-4'>Check Your Business Name for Company Setup in Dubai</h1>
               <p className='text-xl text-gray-400'>Find your perfect UAE business name</p>
             </div>
             <div className='w-full md:w-1/2 lg:w-3/4 mx-auto'>
