2e25231b6773a89361c6606ec3a106cc2ad40dc1
Aitezaz Bilal <aitezazbilal95@gmail.com>
2025-05-07 18:13:55 +0400

    Add CorpTaxCalculatorAnnouncement component to header

---
+++ components/corp-tax-calculator-announcement.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { getVisitorGeoInfo } from '@/lib/helpers'
+
+export const CorpTaxCalculatorAnnouncement = () => {
+  const [showAnnouncement, setShowAnnouncement] = useState(false)
+
+  useEffect(() => {
+    const checkUserLocation = async () => {
+      if (!localStorage.getItem('isUserFromUk')) {
+        const geoInfo = await getVisitorGeoInfo()
+        if (geoInfo && geoInfo.countryCode === 'GB') {
+          localStorage.setItem('isUserFromUk', 'true')
+          setShowAnnouncement(true)
+        } else {
+          localStorage.setItem('isUserFromUk', 'false')
+        }
+      } else if (localStorage.getItem('isUserFromUk') === 'true') {
+        setShowAnnouncement(true)
+      }
+    }
+
+    checkUserLocation()
+  }, [])
+
+  if (!showAnnouncement) return null
+
+  return (
+    <div className='corp-tax-calculator-announcement w-full text-center bg-gray-600 py-1'>
+      <Link href='/uk-corporate-tax-calculator' className='underline text-white'>
+        Check Out Our UK vs Dubai Tax Calculator
+      </Link>
+    </div>
+  )
+}
