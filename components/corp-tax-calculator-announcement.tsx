6d102b71d494372d360f03148f135053d403007d
Raj Karwal <karwalr@hotmail.com>
2025-05-08 10:08:07 +0100

    Padding adjust

--- components/corp-tax-calculator-announcement.tsx
+++ components/corp-tax-calculator-announcement.tsx
@@ -9,8 +9,12 @@ export const CorpTaxCalculatorAnnouncement = () => {
 
   useEffect(() => {
     const checkUserLocation = async () => {
+
+      const geoInfo = await getVisitorGeoInfo()
+        console.log(geoInfo)
       if (!localStorage.getItem('isUserFromUk')) {
         const geoInfo = await getVisitorGeoInfo()
+        console.log(geoInfo)
         if (geoInfo && geoInfo.countryCode === 'GB') {
           localStorage.setItem('isUserFromUk', 'true')
           setShowAnnouncement(true)
@@ -23,12 +27,14 @@ export const CorpTaxCalculatorAnnouncement = () => {
     }
 
     checkUserLocation()
+
+    
   }, [])
 
   if (!showAnnouncement) return null
 
   return (
-    <div className='corp-tax-calculator-announcement w-full text-center bg-gray-600 py-1'>
+    <div className='corp-tax-calculator-announcement w-full text-center bg-gray-600 py-2'>
       <Link href='/uk-corporate-tax-calculator' className='underline text-white'>
         Check Out Our UK vs Dubai Tax Calculator
       </Link>
