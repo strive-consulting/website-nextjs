a564e105286f5bbc049ac2dca8d53960f8069a03
Aitezaz Bilal <aitezazbilal95@gmail.com>
2025-05-08 22:01:57 +0400

    Refactor GET function to retrieve client IP from request headers

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -1,24 +1,27 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET() {
-    try {
-        // Add headers to disable compression and provide user-agent
-        const response = await fetch('https://jsonip.com', {
-            headers: {
-                'Accept-Encoding': 'identity',
-                'User-Agent': 'Mozilla/5.0 Next.js API Route'
-            },
-            cache: 'no-store' // Prevent caching issues
-        });
+export async function GET(req: NextRequest) {
+  try {
+    const clientIp = req.headers.get('x-forwarded-for') || req.ip ///|| req.connection.remoteAddress
 
-        // Get text first to avoid decompression errors
-        const textData = await response.text();
-        // Parse the text data to JSON
-        const data = JSON.parse(textData);
+    return NextResponse.json({ ip: clientIp })
+    //     // Add headers to disable compression and provide user-agent
+    //     const response = await fetch('https://jsonip.com', {
+    //       headers: {
+    //         'Accept-Encoding': 'identity',
+    //         'User-Agent': 'Mozilla/5.0 Next.js API Route',
+    //       },
+    //       cache: 'no-store', // Prevent caching issues
+    //     })
 
-        return NextResponse.json({ ip: data.ip });
-    } catch (error: any) {
-        console.error('Error:', error);
-        return NextResponse.json(error.message);
-    }
+    //     // Get text first to avoid decompression errors
+    //     const textData = await response.text()
+    //     // Parse the text data to JSON
+    //     const data = JSON.parse(textData)
+
+    //     return NextResponse.json({ ip: data.ip })
+  } catch (error: any) {
+    console.error('Error:', error)
+    return NextResponse.json(error.message)
+  }
 }
